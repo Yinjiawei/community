5beec0aeb55d2ad76f727d7645d92ab02dce42bc
jiawei.yin <jiawei.yin@accenture.com>
2020-02-25 17:30:51 +0800

    first commit

---
+++ src/test/java/happiness/jason/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package happiness.jason.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
