d026c1a053ee9e4b015f62eeb1b75ba4cd0a5d81
Yinjiawei <jiawei.yin@hotmail.com>
2020-02-28 16:06:40 +0800

    call github interface

--- src/main/java/happiness/jason/community/controller/AuthorizeController.java
+++ src/main/java/happiness/jason/community/controller/AuthorizeController.java
@@ -1,14 +1,34 @@
 package happiness.jason.community.controller;
 
+import happiness.jason.community.dto.AccessTokenDTO;
+import happiness.jason.community.dto.GithubUser;
+import happiness.jason.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("f7f2611045cd95d27226");
+        accessTokenDTO.setClient_secret("c222c36ef33c90935f124d9447787aa6352866b2");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setState(state);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        System.out.println(githubUser.getName());
+
         return "index";
     }
 }
