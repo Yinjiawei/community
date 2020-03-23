c66bc759e27ca7e12695fe80896653f6f381a70d
Yinjiawei <jiawei.yin@hotmail.com>
2020-03-23 17:59:38 +0800

    reply when not login

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,19 +1,25 @@
-$(function() {
-    $(".btn-comment").on('click', function(){
+$(function () {
+    $(".btn-comment").on('click', function () {
         let questionId = $("#question_id").val();
         let content = $("#question_content").val();
-        let data = JSON.stringify({"parentId": 1, "content":content, "type":1});
+        let data = JSON.stringify({"parentId": 1, "content": content, "type": 1});
         $.ajax({
             type: "POST",
             dataType: "json",
             contentType: "application/json",
             url: "/comment",
             data: data,
-            success: function(response){
+            success: function (response) {
                 console.log(response);
-                if(response.code == 200){
+                if (response.code == 200) {
                     $("#comment_section").hide();
-                }else{
+                } else if (response.code == 2003) {
+                    let isAccept = confirm(response.message);
+                    if (isAccept) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=f7f2611045cd95d27226&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
                     alert(response.message);
                 }
             }
